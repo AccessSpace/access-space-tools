7bfd0f4505df993b8fcdbdf3a057c101f19c7318
martyn.eggleton <martyn.eggleton@f3583e44-0e89-11de-aabd-1bb136c774cb>
2009-07-09 12:15:50 +0000

    Adds the bug reporter

--- trunk/landing/bugreport.php
+++ trunk/landing/bugreport.php
@@ -1,5 +1,33 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+ <title>Bug Reporting</title>
+ <link id="css1" href="layout.css" type="text/css" rel="stylesheet"> 
+
+
+</head>
 <body>
+
+<div id="page">
+ <div id="leftside">
+  <div id="header">
+   <h1><img src="images/access-space.png" alt="" /></h1>
+  </div>
+  <div id="instructions">
+   <h2>Bug Reporting</h2>
+<p>Please use the boxes to the right to tell us :-</p>
+ <ul>
+<li>what has gone wrong?</li>
+<li>
+ can we make the network better for you and your project?</li>
+<ul>
+
+  </div>
+  </div>
+<div id="rightside">
+ <div id="bugreport">
 <?php
 
 @define('CONST_BUGREPORT_EMAIL_ADDR', 'martyn@access-space.org');
@@ -34,33 +62,43 @@ if (isset($_REQUEST['email']))
   else
     {//send email
     $email = $_REQUEST['email'] ;
-    $subject = 'Bug report from '.$_REQUEST['REMOTE_ADDR'] ;
+    $subject = 'Bug report from '.$_SERVER['REMOTE_ADDR'] ;
     
-    $message = $_REQUEST['message'] ;
-    mail("someone@example.com", "Subject: $subject",
+    $message = var_export($_REQUEST, true);//['message'] ;
+    mail(CONST_BUGREPORT_EMAIL_ADDR, "Subject: $subject",
     $message, "From: $email" );
     echo "Thank you for using our mail form";
     }
   }
 else
   {//if "email" is not filled out, display the form
-  echo "<form method='post' action='bugreport.php'>
-  Your Email: <input name='email' type='text' /><br />
-  What are you trying to do?:<br />
-  <textarea name='task' rows='10' cols='40'></textarea><br />
-  What should happen?:<br />
-  <textarea name='task' rows='10' cols='40'></textarea><br />
-  What is happening?:<br />
-  <textarea name='task' rows='10' cols='40'></textarea><br />
+ ?> 
+ <form method='post' action='bugreport.php'>
+  <input name="user" value="<?=$_REQUEST['user']; ?>" type="hidden" />
+  Your Email: <input name='email' type='text' size="50"/><br />
+  What are you trying to use?:<br />
+  <textarea name='task' rows='5' cols='60'></textarea><br />
+  What should it have done?:<br />
+  <textarea name='expected' rows='5' cols='60'></textarea><br />
+  What happened?:<br />
+  <textarea name='actual' rows='5' cols='60'></textarea><br />
   
   Which machines is it affecting:
-  <select></select>
+  <select name="scope">
+   <option>At Least This Machine</option>
+   <option>This Machine Only</option>
+   <option>All Machines</option>
+  </select>
   <br />
   <input type='submit' />
-  </form>";
+  </form>
+
+<?php
   }
 ?>
-
+ </div>
+</div>
+</div>
 </body>
 </html>
 
