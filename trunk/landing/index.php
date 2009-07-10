e8cb68103eba1dc856c4841ea969da1cfea919ba
martyn.eggleton <martyn.eggleton@f3583e44-0e89-11de-aabd-1bb136c774cb>
2009-07-10 15:17:37 +0000

    adds UTF-8 encoding

--- trunk/landing/index.php
+++ trunk/landing/index.php
@@ -1,20 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
 <head>
-
-
-
-
-<title>Access Space</title>
-
-
-<link id="css1" href="layout.css" type="text/css" rel="stylesheet"> 
-
-
-
-
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Access Space</title>
+  <link id="css1" href="layout.css" type="text/css" rel="stylesheet"> 
 </head>
 
 <body>
