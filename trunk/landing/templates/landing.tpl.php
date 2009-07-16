e1df472b104a2c23e58fd8c2385ff649eb007ad9
raj@junglesale.com <raj@junglesale.com@f3583e44-0e89-11de-aabd-1bb136c774cb>
2009-07-16 12:06:42 +0000

    Refactors the index page to use template object.

---
+++ trunk/landing/templates/landing.tpl.php
@@ -0,0 +1,51 @@
+<div id="leftside">
+<div id="header">
+<h1><img src="images/access-space.png" alt="" /></h1>
+</div>
+<div id="links">
+<h2>Useful Links</h2>
+<dl>
+<dt><a href="http://knowhow.access-space.org">The Knowhow Page</a></dt>
+<dd>Helpful knowledge that you will find useful at Access Space.</dd>
+
+<!--
+<dt><a href="#">Book help for your project</a></dt>
+<dd>Arrange a session with a volunteer or staff member.</dd>
+-->
+<dt><a href="http://spacers.lowtech.org">Spacers</a></dt>
+<dd>Wiki of projects by Access Space participants.</dd>
+
+<dt><a href="http://www.access-space.org">Main Site</a></dt>
+<dd>The main site for Access Space.</dd>
+
+<dt><a href="bugreport.php?user=<?=$_REQUEST['user']; ?>">
+Report a bug on this machine</a></dt>
+<dd>If there is something wrong with this machine please use this link to send us an email and report it so we can fix the problem.</dd>
+</dl>
+</div>
+<!--
+<div id="forms">
+<h2>Your Contact Details</h2>
+<form>
+First name:
+<input type="text" name="firstname" />
+<br />
+Last name:
+<input type="text" name="lastname" />
+<br />
+Email:
+<input type="text" name="email" />
+<br />
+<input type="submit" value="Submit" />
+
+</form> 
+</div>
+-->
+</div>
+<div id="rightside">
+<div id="news">
+
+<?=$sNewsContent;?>
+</div>
+</div>
+
