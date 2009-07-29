4250fc6fb785789a18d3721c22eebd7d67e0fa70
martyn.eggleton <martyn.eggleton@f3583e44-0e89-11de-aabd-1bb136c774cb>
2009-07-29 16:22:18 +0000

    Intial commit

---
+++ trunk/one2one/templates/makebooking.tpl.php
@@ -0,0 +1,28 @@
+
+<form>
+<table>
+<tr>
+<td width="33%">
+<select size="20">
+<option>Web Design</option>
+<option>Javascript</option>
+<option>PHP</option>
+<option>Video Editing</option>
+</select>
+</td>
+
+
+<td width="33%">
+<select size="20">
+<option>Web Design</option>
+<option>Javascript</option>
+<option>PHP</option>
+<option>Video Editing</option>
+</select>
+</td>
+
+</form>
+
+  <script type="text/javascript" src="js/mootools.js"></script>
+  <script type="text/javascript" src="js/booking.js"></script>
+
